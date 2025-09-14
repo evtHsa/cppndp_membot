0a4aac97209e9190ae2dc4c83b32c4bafb321816
rocky the squirrel <rocky@bullwinkle.com>
2025-09-14 16:59:10 +0000

    Task 4: moving smart pointers
    instrumentation
    
    unique_ptr(edge): cripple ~ChatLogic to stop crash on close
    
    unique_ptr(edge): turn on one dtor call FROM ~ChatLogic(1 guilty)
    
    remove direct deletion of GraphEdges now handled by smarptrs

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -8,6 +8,7 @@ GraphNode::GraphNode(int id)
 
 GraphNode::~GraphNode()
 {
+    std::cout << "~GraphNode" << std::endl;
     //// STUDENT CODE
     ////
 
@@ -27,9 +28,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge);
+  _childEdges.push_back(edge.get());
 }
 
 //// STUDENT CODE
